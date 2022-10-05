67bf0256dd2c9e20334e2a3b990d006ae5436a60
Adilnurlan <nurlan.adil98@gmail.com>
2022-10-05 23:03:53 +0600

    hm-8(1)

---
+++ src/js/01-gallery.js
@@ -0,0 +1,28 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryItemsMarkup = document.querySelector(".gallery");
+const galleryFoo = galleryMarkup(galleryItems);
+
+galleryItemsMarkup.insertAdjacentHTML("afterbegin", galleryFoo);
+
+function galleryMarkup(galleries) {
+  return galleries
+    .map(({ preview, original, description }) => {
+      return `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`;
+    })
+    .join("");
+}
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});

1ff8d5fa740c7756719b06d9102957239eddbd18
Adilnurlan <nurlan.adil98@gmail.com>
2022-10-10 19:00:26 +0600

    hn-8(2)

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,7 +4,7 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
 const galleryItemsMarkup = document.querySelector(".gallery");
 const galleryFoo = galleryMarkup(galleryItems);
@@ -14,9 +14,9 @@ galleryItemsMarkup.insertAdjacentHTML("afterbegin", galleryFoo);
 function galleryMarkup(galleries) {
   return galleries
     .map(({ preview, original, description }) => {
-      return `<li><a class="gallery__item" href="${original}">
+      return `<a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
-</a></li>`;
+</a>`;
     })
     .join("");
 }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('textarea');
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+populateOutput();
+
+const formData = {};
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.target.reset();
+  localStorage.removeItem('feedback-form-state');
+  console.log(formData);
+}
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function populateOutput() {
+  const savedInput = localStorage.getItem('feedback-form-state');
+  const parsedInput = JSON.parse(savedInput);
+  if (savedInput) {
+    form['email'].value = parsedInput.email;
+    form['message'].value = parsedInput.message;
+  }
+}
